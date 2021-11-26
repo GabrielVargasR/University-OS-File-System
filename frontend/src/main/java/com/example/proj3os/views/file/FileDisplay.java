623d6cafb46412ceae956cddd0507f7568169825
Eduardo <edumadrim@gmail.com>
2021-11-25 22:55:54 -0600

    Fix Modify File

--- frontend/src/main/java/com/example/proj3os/views/file/FileDisplay.java
+++ frontend/src/main/java/com/example/proj3os/views/file/FileDisplay.java
@@ -1,11 +1,9 @@
 package com.example.proj3os.views.file;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import com.example.proj3os.controllers.FileController;
-import com.example.proj3os.helper.Common;
+import com.example.proj3os.helper.IConstants;
 import com.example.proj3os.model.File;
 import com.example.proj3os.model.SessionInfo;
 import com.example.proj3os.views.MainLayout;
@@ -18,15 +16,12 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.router.BeforeEnterEvent;
 import com.vaadin.flow.router.BeforeEnterObserver;
-import com.vaadin.flow.router.BeforeEvent;
-import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteParameters;
 
 @PageTitle("FileView")
 @Route(value = "fileView", layout = MainLayout.class)
-public class FileDisplay extends VerticalLayout implements HasUrlParameter<String> {
+public class FileDisplay extends VerticalLayout implements BeforeEnterObserver {
     
     private File file;
     private SessionInfo session;
@@ -55,6 +50,7 @@ public class FileDisplay extends VerticalLayout implements HasUrlParameter<Strin
         Button saveButton = new Button(new Icon(VaadinIcon.CHECK_CIRCLE));
         saveButton.addClickListener(event ->{
             saveFile();
+            session.setCurrentDirectory(IConstants.ROOT);
             UI.getCurrent().navigate(FilesView.class);
         });
 
@@ -67,13 +63,9 @@ public class FileDisplay extends VerticalLayout implements HasUrlParameter<Strin
         FileController.modifyFile(file.getName(), session.getUsername(), session.getCurrentDirectory(), file.getCreation(), new Date().toString() , file.getExtension(), size, content);
     }
 
-
     @Override
-    public void setParameter(BeforeEvent event, String parameter) {
-
-        file = Common.getBuilder().fromJson(parameter, File.class);
-
-        setup();
-        
+    public void beforeEnter(BeforeEnterEvent event) {
+        file = SessionInfo.getFileToOpen();
+        setup();   
     }
 }
